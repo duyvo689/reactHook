c4351942ffcb2177588e6c6a7578bbe2a55ae78c
duyvo689 <vothanhduy689@gmail.com>
2022-05-11 01:51:32 +0700

    button event onclick

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from '../src/views/Nav'
 
-function App() {
+const App = () => {
+  let name = 'Duy Vo';
+
+  const handleEvenClick = () => {
+    console.log('>click me')
+  }
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chao anh {name}</h1>
+        <input type="text" />
+        <button type='button' onClick={() => handleEvenClick()}>Click me</button>
       </header>
     </div>
   );
