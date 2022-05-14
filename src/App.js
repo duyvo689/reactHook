99d1fde826ad6f60c916bcd51db845c17aaef72d
duyvo689 <vothanhduy689@gmail.com>
2022-05-14 17:24:38 +0700

    list todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from '../src/views/Nav'
+import React, { useState } from 'react';
 
 const App = () => {
-  let name = 'Duy Vo';
+  const [name, setName] = useState('duyvo');
+  const [address, setAddress] = useState('');
+  const [todos, setTodos] = useState(
+    [
+      { id: 'todo1', title: 'Home word' },
+      { id: 'todo2', title: 'Play game' }
+    ]
+  )
 
-  const handleEvenClick = () => {
-    console.log('>click me')
+  const handleOnChange = (event) => {
+    setAddress(event.target.value)
+  }
+  const handleEvenClick = (event) => {
+    let newTodo = { id: 'acd', title: address }
+    setTodos([...todos, newTodo])
+    setAddress('')
   }
   return (
     <div className="App">
@@ -14,10 +27,17 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Chao anh {name}</h1>
-        <input type="text" />
-        <button type='button' onClick={() => handleEvenClick()}>Click me</button>
-      </header>
-    </div>
+        <div className='todo-container'>
+          {todos.map(todo => {
+            return (
+              <li className='todo-chile' key={todo.id}>{todo.title}</li>
+            )
+          })}
+        </div>
+        <input type="text" value={address} onChange={(event) => handleOnChange(event)} />
+        <button type='button' onClick={(event) => handleEvenClick(event)}>Click me</button>
+      </header >
+    </div >
   );
 }
 
